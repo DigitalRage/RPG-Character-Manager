6a34b03230cf06ae908922185760ba8f07f07722
DigitalRage <blaine.buckler@ucas-edu.net>
2026-01-21 09:50:06 -0700

    Placebo Code

---
+++ skill+stat_manager.py
@@ -0,0 +1,23 @@
+#Put inside file function
+    #use set level function
+
+    #While editing
+    #Characters starting starts are in nested dictionary
+
+    #Player choses single or all stat dit by adding or removing levels, or editing a single stat
+
+    #Based on level stat, other stats are multiplied for actual stat
+
+    #Atributes for levels
+
+    #Change single stat
+    #Saves to added stats bar
+
+
+    #Choice to add character skills based on class and level
+    #If the stat is available (not already used and claimed) and a slot is left, then add the new skills to their inventory; 
+    #Else, go back to show skills
+
+    #Choice to remove character skills
+    #If no skill to remove, go back to edit
+    #else, if the name of skill equals the input of user, remove the skill. 
