b40ee66c8fc380b4c6dc2dbf32a0eeb5f519f210
DigitalRage <blaine.buckler@ucas-edu.net>
2026-01-22 09:12:47 -0700

    Edits

--- skill+stat_manager.py
+++ skill+stat_manager.py
@@ -2,22 +2,24 @@
     #use set level function
 
     #While editing
-    #Characters starting starts are in nested dictionary
+        #Characters starting starts are in nested dictionary
 
-    #Player choses single or all stat dit by adding or removing levels, or editing a single stat
+        #Player choses single or all stat dit by adding or removing levels, or editing a single stat
 
-    #Based on level stat, other stats are multiplied for actual stat
+        #Based on level stat, other stats are multiplied for actual stat
 
-    #Atributes for levels
+        #Atributes for levels
 
-    #Change single stat
-    #Saves to added stats bar
+        #Change single stat
+        #Saves to added stats bar
 
 
-    #Choice to add character skills based on class and level
-    #If the stat is available (not already used and claimed) and a slot is left, then add the new skills to their inventory; 
-    #Else, go back to show skills
+        #Choice to add character skills based on class and level
+        #If the stat is available (not already used and claimed) and a slot is left, then add the new skills to their inventory; 
+        #Else, go back to show skills
 
-    #Choice to remove character skills
-    #If no skill to remove, go back to edit
-    #else, if the name of skill equals the input of user, remove the skill. 
+        #Choice to remove character skills
+        #If no skill to remove, go back to edit
+        #else, if the name of skill equals the input of user, remove the skill. 
+
+        #if editing equals false, then break
