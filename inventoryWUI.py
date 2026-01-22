3335016ac7e548afaf0ce580d66afc1c70ee6a09
Will5I <will.malloch@ucas-edu.net>
2026-01-22 09:43:00 -0700

    pseudocode done and main function made

--- inventoryWUI.py
+++ inventoryWUI.py
@@ -1,7 +1,7 @@
 # WM 1st pseudocode
 
 #UI
-    #Create a function for the main interface, just named interface()
+    #Create a function for the main interface, just named main()
     #Have it greet them to the program.
     #Start a while true loop that will keep the code going until the user chooses to exit.
     #They will be asked what they want to do, 1.Make a character 2.Edit a character 3.Search for a character 4.Exit
@@ -19,15 +19,22 @@
         #Armor would also be class restricted, based on what was choosen in creator.
             #There would be a few options for each class, with them affecting defense, evasion, speed, and spirit differently
     #Make a function for creating a character inventory.
-        #It would start by taking the class, and using some if else it would tick some things, and then only print stuff they can use, alongside the things stats.
+def new_inven():
+    pass
+        #It will search for keywords like warrior or mage in their class and mark variables as true where needed.
+        #It will print all the valid items for them
         #It would start with weapons and the armor, and then finally do equipment. a variable would keep track of how many equipment they choose, so it will end when they get all 4.
         #All of these would loop until a variable for each becomes true, and lets it move on.
         #It would finally return the character dictionary at the end.
+        #When they choose an item, it will use that to search for the item, and if it exists, it will append it to the character dictionaray.
     #Make a fucntion for editing an already made character
+def edit_inven():
+    pass
         #It will ask if they want to remove an item, or add an item.
             #If they choose either, it will check to see if they have either an item to remove, or an open slot to add to.
             #If they do, they would both ask what item, either to remove, which it would print their items they have, or what they want to add, printing the right items, taking restrictions into account.
+            #When they choose a valid item, it would fill a dictionary with a empty place holder if they removed, and if they added it would insert the item into the correct slot.
             #It would return back to the main chunk function of character editer, by Mirai.
     #Function names
-    #newInven()
-    #editInven()
+    #new_inven()
+    #edit_inven()
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #Import other files for functions
 
+import sys
 #Define main
-
+def main():
+    print("Welcome to the Rpg Character Manager. You can create, edit, and search for characters here.")
+    while True:
+        choice = input("What would you like to do?\n1.Create a new character\n2.Edit an already made character\n3.Search through characters\n4.Exit\n")
+        if choice == '1':
+            character_creator()
+        elif choice == '2':
+            character_editor()
+        elif choice == '3':
+            insertsearchfunc
+        if choice == '4':
+            print("Goodbye")
+            sys.exit()
+        else:
+            print("Invalid choice, try again")
 #Run Main
