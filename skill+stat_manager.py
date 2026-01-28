c4c5fce65306c51edf7ea5dcebcc8bc65d545ff2
DigitalRage <blaine.buckler@ucas-edu.net>
2026-01-28 09:50:14 -0700

    Good Push Message

--- skill+stat_manager.py
+++ skill+stat_manager.py
@@ -1,5 +1,7 @@
+#BB 1st Skill & Stat Manager Project
 #Put inside file function
     #use set level function
+from character_manager import *
 def setup_char_value(): 
     #While editing
     while True:
@@ -9,19 +11,42 @@ def setup_char_value():
                 'Warrior': { 'Stats': {'MP': 10, 'HP': 80, 'Str': 10, 'Atk': 7, 'Def': 10, 'Mag': 0, 'Spr': 3, 'Acc': 70, 'Spd': 15, 'Evs': 15, 'Lvl': 1}, 'Atributes': {'Jump': 21, 'Defence': 37}}, 
                 'Theif': { 'Stats': {'MP': 5, 'HP': 60, 'Str': 5, 'Atk': 9, 'Def': 7, 'Mag': 0, 'Spr': 6, 'Acc': 65, 'Spd': 25, 'Evs': 25, 'Lvl': 1}, 'Atributes': {'Steal': 1, 'Mug': 30, 'Taunt': 15, 'Cheer': 21}}, 
                 'White Mage': { 'Stats': {'MP': 15, 'HP': 50, 'Str': 5, 'Atk': 5, 'Def': 5, 'Mag': 10, 'Spr': 9, 'Acc': 40, 'Spd': 25, 'Evs': 30, 'Lvl': 1}, 'Atributes': {'Cure': 7, 'Cura': 20, 'Curaga': 40, 'Life': 25, 'Full Life': 50, 'Esuna': 15}}
-
         }
+
+        added_dic = {
+            'Stats': {'MP': 0, 'HP': 0, 'Str': 0, 'Atk': 0, 'Def': 0, 'Mag': 0, 'Spr': 0, 'Acc': 0, 'Spd': 0, 'Evs': 0, 'Lvl': 0}
+        } 
+
+        #player is asked for name of character
+        name = input('Who are you editing? \n>')
+        char = character_manager.char_return()
+        clas = char[name]['class']
         #Player choses single or all stat editor, then opens adding or removing level function, or editing a single stat function
         choice = input('Do you want to: \n1. Edit single stat\n2. Edit level\n3. Edit attributes')
         #Multiply by level function:
         def mult_level(): 
+            #Based on level stat, other stats are multiplied by 0.1x for actual stat
             new_level = input("What is the character Level?\n>")
-            char_table = char_table_lv1*[new_level]
-        #Based on level stat, other stats are multiplied by 0.1x for actual stat
+            char_table_lv1[clas]['stats']['lvl'] = new_level
+            for key in char_table_lv1[clas]['Stats']: 
+                if key == 'level': continue
+                else: 
+                    for i in range(new_level):
+                        key = key*i
 
         #Single stat Function: 
-        #Asks player which stat
-        #Asks user the value to change by
+        def add_single():
+            #Asks player which stat 
+            stat_chose = input('What stat will you edit?\n>')
+            #Asks user the value to change by
+            while True: 
+                amount = input('How much?')
+                try: int(amount)
+                except ValueError: 
+                    print('try again')
+                
+
+            char_table_lv1
         #Change single stat
         #Saves to added stats bar in dictionary
 
