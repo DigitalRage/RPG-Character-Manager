7f9920fe5045b57faa8c6ab110b015e0f0a2b884
Will5I <will.malloch@ucas-edu.net>
2026-01-29 09:53:23 -0700

    edited main to make if function

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #Import other files for functions
 
 import sys
+from char_manager import *
+from character_search import *
+from inventoryWUI import *
+from skill_stat_manager import *
 #Define main
 def main():
     print("Welcome to the Rpg Character Manager. You can create, edit, and search for characters here.")
     while True:
         choice = input("What would you like to do?\n1.Create a new character\n2.Edit an already made character\n3.Search through characters\n4.Exit\n")
         if choice == '1':
-            character_creator()
+            create_character(chracters,race_options,class_options)
         elif choice == '2':
-            character_editor()
+            edit_character(chracters)
         elif choice == '3':
-            insertsearchfunc
+            char_search()
         if choice == '4':
             print("Goodbye")
             sys.exit()
