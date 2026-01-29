f2130fd6eb926718120c804f6ae8522f1c5eb56e
guy-glitch <warren.gibson@ucas-edu.net>
2026-01-29 09:13:51 -0700

    character search fixed import

--- character_search.py
+++ character_search.py
@@ -1,7 +1,7 @@
 #WG_CP2 character search for group
 #The code for searching though the characters
 #from character creator import character return
-from char_manager_pseudocode import char_return
+from char_manager import char_return
 
 #from main import main
 from main import main
